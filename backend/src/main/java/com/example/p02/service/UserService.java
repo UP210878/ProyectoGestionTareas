ad519d463185064048c4fb6a66a54f03defdb532
V1nc3nt <up210878@alumnos.upa.edu.mx>
2024-07-08 13:26:49 -0600

    gradle security module

--- backend/src/main/java/com/example/p02/service/UserService.java
+++ backend/src/main/java/com/example/p02/service/UserService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.example.p02.model.User;
 import com.example.p02.repository.UserRepository;
 
