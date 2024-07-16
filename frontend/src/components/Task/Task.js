574bb9500233dc07dbaa43c418b8c6c4870c07c9
UP210685 <up210685@alumnos.upa.edu.mx>
2024-07-16 12:14:28 -0600

    "Category frontend"

--- frontend/src/components/Task/Task.js
+++ frontend/src/components/Task/Task.js
@@ -2,11 +2,25 @@ import React from 'react';
 import './Task.css';
 
 class Task extends React.Component {
-  render(){
+  render() {
     return (
-      <div><p>Activity</p></div>
+      <div className="task">
+        <h3>Task Title</h3>
+        <div className="activity">
+          <input type="checkbox" id="activity1" />
+          <label htmlFor="activity1">Activity 1</label>
+        </div>
+        <div className="activity">
+          <input type="checkbox" id="activity2" />
+          <label htmlFor="activity2">Activity 2</label>
+        </div>
+        <button className="edit-btn">Edit</button>
+        <div className="complete-toggle">
+          <label>Complete</label>
+          <input type="checkbox" />
+        </div>
+      </div>
     );
   }
 }
-
 export default Task;
