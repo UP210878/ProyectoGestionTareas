d589b0176c347c4822fa9312310135cca71cd07b
Vinc3nt <up210878@alumnos.upa.edu.mx>
2024-07-17 17:41:35 -0600

    LogOut done

--- frontend/src/components/Login/Logout.js
+++ frontend/src/components/Login/Logout.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import { Typography, Button, Container, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React, {useEffect, useContext} from 'react';
+import { Typography, Container, Box, CircularProgress } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Common';
+
 
 
 const Logout = () => {
+  const navigate = useNavigate();
+  const { setIsAuthenticated } = useContext(AuthContext);
+  const logToken = sessionStorage.getItem('token');
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsAuthenticated(false);
+      navigate('/login');
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [navigate,setIsAuthenticated]);
+
+  if (logToken!==null) {
+    sessionStorage.removeItem('token');
+  }
 
   return (
-    <div>
-      <a>
-        Logout
-      </a>
-    </div>
+    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '100px' }}>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          You've been logged out.
+        </Typography>
+        <Typography variant="h3" component="h2" gutterBottom>
+          Redirecting...
+        </Typography>
+        <CircularProgress color='primary'/>
+      </Box>
+    </Container>
   );
 }
 
