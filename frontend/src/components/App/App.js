2a1b232bb5745c522ae003d47bcc589ce5d3a8f0
Vinc3nt <up210878@alumnos.upa.edu.mx>
2024-07-18 18:16:24 -0600

    Reacomodo BrowserRouter a index.js

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,20 +1,15 @@
 import './App.css';
 import Main from '../Main';
 import Nav from '../Nav';
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '../Common';
 
 function App() {
   return (
 <div>
-  <BrowserRouter>
   <AuthProvider>
   <Nav />
   <Main />
   </AuthProvider>
-  
-  </BrowserRouter>
-
 </div>
   );
 }
