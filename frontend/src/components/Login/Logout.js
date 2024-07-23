b03c9a8bb531bdac711db30dd95d9d0273aed933
Vinc3nt <up210878@alumnos.upa.edu.mx>
2024-07-23 14:03:55 -0600

    Fixed Logout Error about misplaced context

--- frontend/src/components/Login/Logout.js
+++ frontend/src/components/Login/Logout.js
@@ -1,27 +1,26 @@
-import React, {useEffect, useContext} from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Typography, Container, Box, CircularProgress } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../Common';
 
-
-
 const Logout = () => {
   const navigate = useNavigate();
   const { setIsAuthenticated } = useContext(AuthContext);
-  const logToken = sessionStorage.getItem('token');
 
-
-  setIsAuthenticated(false);
   useEffect(() => {
+    const logToken = sessionStorage.getItem('token');
+    if (logToken !== null) {
+      sessionStorage.removeItem('token');
+    }
+    
+    setIsAuthenticated(false);
+    
     const timer = setTimeout(() => {
       navigate('/login');
     }, 2000);
+    
     return () => clearTimeout(timer);
-  }, [navigate]);
-
-  if (logToken!==null) {
-    sessionStorage.removeItem('token');
-  }
+  }, [navigate, setIsAuthenticated]);
 
   return (
     <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '100px' }}>
@@ -31,14 +30,15 @@ const Logout = () => {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-        }}>
+        }}
+      >
         <Typography variant="h2" component="h1" gutterBottom>
           You've been logged out.
         </Typography>
         <Typography variant="h3" component="h2" gutterBottom>
           Redirecting...
         </Typography>
-        <CircularProgress color='primary'/>
+        <CircularProgress color="primary" />
       </Box>
     </Container>
   );
