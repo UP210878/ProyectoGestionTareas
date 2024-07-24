952ab59ce8b686b35c5cff26e6a34620ba076ac9
Vinc3nt <up210878@alumnos.upa.edu.mx>
2024-07-24 07:42:21 -0600

    Persistant mode via localstorage.

--- frontend/src/components/Common/ModeContext.js
+++ frontend/src/components/Common/ModeContext.js
@@ -1,9 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 const ModeContext = createContext();
 
 const ModeProvider = ({ children }) => {
-    const [isDarkMode, setDarkMode] = useState(false);
+    const [isDarkMode, setDarkMode] = useState(() => {
+      const localMode = localStorage.getItem('darkMode');
+      return localMode ? JSON.parse(localMode) : false;
+    });
+
+    useEffect(() => {
+      localStorage.setItem('darkMode',JSON.stringify(isDarkMode));
+    }, [isDarkMode]);
 
     return (
       <ModeContext.Provider value={{ isDarkMode, setDarkMode }}>
