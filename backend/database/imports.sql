a6a568b093976627be732139ddff8090333e2914
Vinc3nt <up210878@alumnos.upa.edu.mx>
2024-07-28 15:34:34 -0600

    new import to test assigneuser

--- backend/database/imports.sql
+++ backend/database/imports.sql
@@ -7,6 +7,8 @@ INSERT INTO task(categoryId,taskName,dueDate) VALUES (1,"Tarea2",'2024-10-10');
 INSERT INTO task(categoryId,taskName,dueDate) VALUES (3,"Tarea3",'2024-10-10');
 
 INSERT INTO activity(taskId,completed,activityName) VALUES (1,true,"Actividad1");
+INSERT INTO activity(taskId,completed,activityName,assignedUser) VALUES (1,false,"Actividad2",3);
+
 
 
 INSERT INTO category(userId,categoryName) VALUES (2,"CategoriaPrueba5");
