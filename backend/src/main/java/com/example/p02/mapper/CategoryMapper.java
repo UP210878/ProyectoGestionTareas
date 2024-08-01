1acb17df0cf8cd4bd7bdbcb5d851908c2fe65db6
Adriana <112640854+UP210692@users.noreply.github.com>
2024-08-01 15:33:12 -0600

    Update categorymapper.java

--- backend/src/main/java/com/example/p02/mapper/CategoryMapper.java
+++ backend/src/main/java/com/example/p02/mapper/CategoryMapper.java
@@ -1,5 +1,28 @@
 package com.example.p02.mapper;
 
-public class CategoryMapper {
-    
+import com.example.p02.dto.CategoryAddDTO;
+import com.example.p02.dto.UserRegisterDTO;
+import com.example.p02.model.Category;
+
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import java.util.ArrayList;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(target = "tasks", ignore = true)
+    Category toCategory(CategoryAddDTO categoryaddDTO);
+
+    CategoryAddDTO toCategoryAddDTO(Category category);
+
+    @AfterMapping
+    default void initializeTasks(@MappingTarget Category category) {
+        if (category.getTasks() == null) {
+            category.setTasks(new ArrayList<>());
+        }
+    }
 }
