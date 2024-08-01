dc126521caa4ebb5917474fce3f001859a4e01a9
UP210685 <up210685@alumnos.upa.edu.mx>
2024-08-01 10:22:18 -0600

    Query findIdByUsername & findUsernames

--- backend/src/main/java/com/example/p02/repository/UserRepository.java
+++ backend/src/main/java/com/example/p02/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.p02.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,4 +14,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
         @Query(value = "SELECT * FROM user WHERE email = ?1", nativeQuery = true)
         User findByEmail(String email);
+
+        @Query(value = "SELECT userId FROM user WHERE username = ?1", nativeQuery = true)
+        Long findIdByUsername(String username);
+
+        @Query(value = "SELECT username FROM user", nativeQuery = true)
+        List<String> findUsernames();
 }
