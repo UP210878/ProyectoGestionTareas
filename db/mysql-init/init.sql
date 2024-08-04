8289112fb4b4af6d18812e7c153b35e86cf4ee69
UP210752 <up210752@alumnos.upa.edu.mx>
2024-08-03 22:57:00 -0600

    Creación de los tres contenedores (Front, Back y MySQL) puertos (3000, 8080 y 3306 respectivamente) NOTA: Estos cambios solo representan la creación de los 3 contenedores gracias al docker-compose.yml, estos cambios son antes de la implementación del Droplet

--- db/mysql-init/init.sql
+++ db/mysql-init/init.sql
@@ -1,16 +1,12 @@
 CREATE DATABASE IF NOT EXISTS TODOAPP;
 
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-
-GRANT ALL PRIVILEGES ON TODOAPP.* TO 'admin'@'localhost';
-
 USE TODOAPP;
 
-CREATE TABLE `user` (
+CREATE TABLE user (
     userId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(70) NOT NULL UNIQUE,
-    password VARCHAR(70) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -18,23 +14,23 @@ CREATE TABLE `user` (
 CREATE TABLE category (
     categoryId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     userId INT NOT NULL,
-    categoryName VARCHAR(70) NOT NULL,
+    categoryName VARCHAR(150) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT ct_id_u_fk FOREIGN KEY (userId)
-    references user (userId)
+    REFERENCES user (userId)
 );
 
 CREATE TABLE task (
     taskId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     categoryId INT NOT NULL,
-    taskName VARCHAR(70) NOT NULL,
+    taskName VARCHAR(150) NOT NULL,
     dueDate DATE,
     completed BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT tk_id_c_fk FOREIGN KEY (categoryId)
-    references category (categoryId)
+    REFERENCES category (categoryId)
 );
 
 CREATE TABLE activity (
@@ -42,26 +38,11 @@ CREATE TABLE activity (
     taskId INT NOT NULL,
     assignedUser INT DEFAULT NULL,
     completed BOOLEAN DEFAULT FALSE,
-    activityName VARCHAR(70) NOT NULL,
+    activityName VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT act_id_t_fk FOREIGN KEY (taskId)
-    references task (taskId),
+    REFERENCES task (taskId),
     CONSTRAINT act_id_u_fk FOREIGN KEY (assignedUser)
-    references user (userId)
+    REFERENCES user (userId)
 );
-
-INSERT INTO category(userId,categoryName) VALUES (1,"CategoriaPrueba1");
-INSERT INTO category(userId,categoryName) VALUES (1,"CategoriaPrueba2");
-INSERT INTO category(userId,categoryName) VALUES (1,"CategoriaPrueba3");
-
-
-INSERT INTO task(categoryId,taskName,dueDate) VALUES (1,"Tarea1",'2024-10-10');
-INSERT INTO task(categoryId,taskName,dueDate) VALUES (1,"Tarea2",'2024-10-10');
-INSERT INTO task(categoryId,taskName,dueDate) VALUES (3,"Tarea3",'2024-10-10');
-
-INSERT INTO activity(taskId,completed,activityName) VALUES (1,true,"Actividad1");
-INSERT INTO activity(taskId,completed,activityName,assignedUser) VALUES (1,false,"Actividad2",3);
-
-
-INSERT INTO category(userId,categoryName) VALUES (2,"CategoriaPrueba5");
